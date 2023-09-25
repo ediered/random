4ef3da2edf72be232c8977473190da64ffac9025
ediered <51175666+ediered@users.noreply.github.com>
2023-09-25 10:14:38 +0200

    Add files via upload

---
+++ e1_beginnen.mjs
@@ -0,0 +1,7 @@
+let name = 'Eden'; 
+let age = '19'; 
+let favoriteGame = 'Super Mario 64'; 
+
+
+
+console.log('Hallo '+ name +', ik hoor dat je '+ age +' jaar oud bent en dat je lievelingsgame '+ favoriteGame +' is.');
